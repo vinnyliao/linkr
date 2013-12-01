b3a7d2bfd2dbc6af92e75da2023e4cf545db48dd
Vincent Liao <vincentliao@outlook.com>
2013-12-01 11:08:56 -0500

    fix port

--- server.js
+++ server.js
@@ -14,5 +14,6 @@ app.post('/users', users.addUser);
 app.get('/users/:id', users.getUser);
 app.get('/users', users.getUsers);
 
-app.listen(3000);
-console.log('Listening on port 3000');
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('Listening on port ' + port);
